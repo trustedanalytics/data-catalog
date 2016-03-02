1780fc04f326b2a83cdbcbde883dddaef429a524
mateusz-ciesielski <mateusz.j.ciesielski@intel.com>
2016-03-02 11:19:45 +0100

    Clone pynats over https

--- cf_build.sh
+++ cf_build.sh
@@ -1,5 +1,5 @@
 #/bin/bash
 set -e
 mkdir -p vendor/
-pip install --download vendor/ -r requirements-normal.txt
+pip install --exists-action=w --download vendor/ -r requirements-normal.txt
 pip install --download vendor/ -r requirements-native.txt --no-use-wheel
